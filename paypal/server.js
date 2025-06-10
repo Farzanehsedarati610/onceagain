c9b11287d84f18fab9190ca4840bd14ebeeffff7
Farzanehsedarati610 <farzanehsedarati6@gmail.com>
2025-06-09 22:17:52 -0400

    f

--- paypal/server.js
+++ paypal/server.js
@@ -4,6 +4,7 @@ const paymentRoutes = require('./routes/payment');
 
 const app = express();
 app.use(bodyParser.json());
+const paymentRoutes = require('./routes/payment');
 app.use('/api/payment', paymentRoutes);
 
 app.listen(3000, () => console.log('Server running on port 3000'));
