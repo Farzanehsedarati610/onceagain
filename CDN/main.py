639315a7015d84f50aee4ce00823fe7ec1c861b7
Farzaneh Sedarati <farzanehsedarati6@gmail.com>
2025-07-01 13:04:46 -0400

    issing

--- CDN/main.py
+++ CDN/main.py
@@ -1,4 +1,13 @@
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI, Request
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:8080"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 from pydantic import BaseModel
 import hashlib, json
 
